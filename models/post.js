ef7ba8f2c8da82eea280b286e1957da6f89fb081
ireneshih <90667518+ireneshih@users.noreply.github.com>
2024-05-05 21:52:24 +0800

    移除_v

--- models/post.js
+++ models/post.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose");
-const postSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "貼文名稱未填寫"],
+const postSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "貼文名稱未填寫"],
+    },
+    type: {
+      type: String,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    createAt: {
+      type: Date,
+      default: Date.now,
+      select: false,
+    },
+    content: {
+      type: String,
+      required: [true, "貼文內容未填寫"],
+    },
   },
-  type: {
-    type: String,
-  },
-  image: {
-    type: String,
-    default: "",
-  },
-  createAt: {
-    type: Date,
-    default: Date.now,
-    select: false,
-  },
-  content: {
-    type: String,
-    required: [true, "貼文內容未填寫"],
-  },
-});
+  { versionKey: false }
+);
 
 const Post = mongoose.model("post", postSchema);
 
